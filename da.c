1cf18f03b4d47c01c1e08ffb3fbfc5be8be0f816
Shlabadee <84961162+Shlabadee@users.noreply.github.com>
2025-02-15 13:20:41 -0500

    Made major modifications. Removed the shuffle functions, optimized the insert function, and renamed the struct to DynamicArray.

--- da.c
+++ da.c
@@ -1,288 +1,226 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "da.h"
-#include "da_rand.h"
-#include "dbgprint.h"
+
+// clang-format off
+#ifdef DEBUG
+    #include <stdio.h>
+    #define DBGPRINT printf
+#else
+    #define DBGPRINT(...) do {} while(0)
+#endif
+// clang-format on
+
+static bool h_memlap(const void* ptr1, size_t size1, const void* ptr2, size_t size2)
+{
+	const char* start1 = (const char*)ptr1;
+	const char* end1 = start1 + size1;
+	const char* start2 = (const char*)ptr2;
+	const char* end2 = start2 + size2;
+	return (start1 < end2) && (start2 < end1);
+}
 
 /**
- * Initializes the dynamic array. `element_size` refers to the `sizeof(type_t)`. `initial_capacity` is
- * total size of the data array (i.e. `_capacity` times `element_size`).
+ * Initializes the dynamic array. `element_size` refers to the `sizeof(type_t)`.
+ * `initial_capacity` is total size of the data array (i.e. `_capacity` times `element_size`).
  */
-int da_initialize(struct dynamic_array* da, size_t initial_capacity, size_t element_size)
+int da_initialize(DynamicArray* da, size_t initial_capacity, size_t element_size)
 {
-    da->_data = calloc(initial_capacity, element_size);
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	da->_data = calloc(initial_capacity, element_size);
+	if (da->_data == NULL)
+	{
+		return 1;
+	}
 
-    da->_count = 0;
-    da->_capacity = initial_capacity;
-    da->_element_size = element_size;
-    da->default_expand_amount = 8;
+	da->_count = 0;
+	da->_capacity = initial_capacity;
+	da->_element_size = element_size;
+	da->default_expand_amount = 8;
 
-    return 0;
+	return 0;
 }
 
 /**
  * Frees the dynamic array and sets the `_data` member to `NULL`.
  */
-int da_free(struct dynamic_array* da)
+int da_free(DynamicArray* da)
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL)
+	{
+		return 1;
+	}
 
-    free(da->_data);
-    da->_count = 0;
-    da->_capacity = 0;
-    da->_element_size = 0;
+	free(da->_data);
+	da->_data = NULL;
+	da->_count = 0;
+	da->_capacity = 0;
+	da->_element_size = 0;
 
-    return 0;
+	return 0;
 }
 
-static void* da_retrieve(struct dynamic_array* da, size_t index)
+static void* da_retrieve(DynamicArray* da, size_t index)
 {
-    if (da->_data == NULL)
-    {
-        return NULL;
-    }
+	if (da->_data == NULL)
+	{
+		return NULL;
+	}
 
-    return (char*)da->_data + (da->_element_size * index);
+	return (char*)da->_data + (da->_element_size * index);
 }
 
 /**
  * Returns the element at `index` as a `void*`. Be sure to cast return value.
  */
-void* da_at(struct dynamic_array* da, size_t index)
+void* da_at(DynamicArray* da, size_t index)
 {
-    if (da->_data == NULL)
-    {
-        return NULL;
-    }
+	if (da->_data == NULL)
+	{
+		return NULL;
+	}
 
-    if (index >= da->_count)
-    {
-        return NULL;
-    }
+	if (index >= da->_count)
+	{
+		return NULL;
+	}
 
-    return da_retrieve(da, index);
+	return da_retrieve(da, index);
 }
 
 /**
  * Expands dynamic array by the given amount. The amount shall be based on
  * the element size, not the size in bytes.
  */
-int da_expand(struct dynamic_array* da, size_t expand_amount)
+int da_expand(DynamicArray* da, size_t expand_amount)
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL)
+	{
+		return 1;
+	}
 
-    void* temp_ptr;
-    temp_ptr = realloc(da->_data, (da->_capacity * da->_element_size) + (expand_amount * da->_element_size));
+	void* temp_ptr;
+	temp_ptr = realloc(da->_data, (da->_capacity * da->_element_size)
+	                                  + (expand_amount * da->_element_size));
 
-    if (temp_ptr == NULL)
-    {
-        return 2;
-    }
+	if (temp_ptr == NULL)
+	{
+		return 2;
+	}
 
-    da->_data = temp_ptr;
-    da->_capacity += expand_amount;
+	da->_data = temp_ptr;
+	da->_capacity += expand_amount;
 
-    return 0;
+	return 0;
 }
 
 /**
  * Shrinks dynamic array to `_count`, freeing unused memory.
  */
-int da_shrink(struct dynamic_array* da)
+int da_shrink(DynamicArray* da)
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL)
+	{
+		return 1;
+	}
 
-    if (da->_count == 0)
-    {
-        return 2;
-    }
+	if (da->_count == 0)
+	{
+		return 2;
+	}
 
-    size_t new_capacity = da->_count;
-    void* temp_ptr;
-    temp_ptr = realloc(da->_data, new_capacity * da->_element_size);
+	size_t new_capacity = da->_count;
+	void* temp_ptr;
+	temp_ptr = realloc(da->_data, new_capacity * da->_element_size);
 
-    if (temp_ptr == NULL)
-    {
-        return 3;
-    }
+	if (temp_ptr == NULL)
+	{
+		return 3;
+	}
 
-    da->_data = temp_ptr;
-    da->_capacity = new_capacity;
+	da->_data = temp_ptr;
+	da->_capacity = new_capacity;
 
-    return 0;
+	return 0;
 }
 
 /**
  * Adds given element to the end of the dynamic array. This will automatically
  * expand the dynamic array if necessary based on the `default_expand_amount` member.
  */
-int da_push(struct dynamic_array* da, void* element)
+int da_push(DynamicArray* da, void* element)
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL)
+		return 1;
 
-    if (da->_count >= da->_capacity)
-    {
-        if (da_expand(da, da->default_expand_amount))
-        {
-            return 2;
-        }
-    }
+	if (da->_count >= da->_capacity)
+	{
+		if (da_expand(da, da->default_expand_amount))
+			return 2;
+	}
 
-    void* temp_ptr;
-    temp_ptr = da_retrieve(da, da->_count);
+	void* temp_ptr;
+	temp_ptr = da_retrieve(da, da->_count);
 
-    if (memcpy(temp_ptr, element, da->_element_size) == NULL)
-    {
-        // ???
-        return 3;
-    }
+	if (memcpy(temp_ptr, element, da->_element_size) == NULL)
+		return 3; // A weird error
 
-    ++da->_count;
+	++da->_count;
 
-    return 0;
+	return 0;
 }
 
 /**
  * Removes the last element from the dynamic array.
  */
-int da_pop(struct dynamic_array* da)
+int da_pop(DynamicArray* da)
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL || da->_count == 0)
+		return 1;
 
-    --da->_count;
+	--da->_count;
 
-    return 0;
+	return 0;
 }
 
-int da_insert(struct dynamic_array* dest, struct dynamic_array* insrt, size_t pos)
+int da_insert(DynamicArray* dest, DynamicArray* src, size_t pos)
 {
-    if (dest->_data == NULL)
-    {
-        return 1;
-    }
+	if (!dest || !src || pos > dest->_count)
+		return 1;
 
-    if (insrt->_data == NULL)
-    {
-        return 2;
-    }
+	// Detect memory overlap
+	if (h_memlap(dest->_data, dest->_count * dest->_element_size, src->_data,
+	             src->_count * src->_element_size))
+		return 2;
 
-    if (dest->_element_size != insrt->_element_size)
-    {
-        return 3;
-    }
+	// Expand if necessary
+	if (dest->_count + src->_count > dest->_capacity)
+	{
+		if (da_expand(dest, src->_count) != 0)
+			return 1;
+	}
 
-    // If requested position is greater than the array size
-    // set position to the end
-    if (pos >= dest->_count)
-    {
-        pos = dest->_count;
-    }
+	// Perform memory move and insert
+	memmove((char*)dest->_data + (pos + src->_count) * dest->_element_size,
+	        (char*)dest->_data + pos * dest->_element_size,
+	        (dest->_count - pos) * dest->_element_size);
 
-    void* first_partition = NULL;
-    void* second_partition = NULL;
-    void* combined_array = NULL;
+	memcpy((char*)dest->_data + pos * dest->_element_size, src->_data,
+	       src->_count * src->_element_size);
 
-    size_t size_fp = 0;
-    size_t size_sp = 0;
-    size_t size_ip = 0;
-    size_t current_bpos = 0; // current position in bytes
-    size_t new_count;
-    new_count = dest->_count + insrt->_count;
-
-    combined_array = malloc(new_count * dest->_element_size);
-
-    if (combined_array == NULL)
-    {
-        return 4;
-    }
-
-    // Copy first partition into combined array
-    // if requested position allows for it
-    if (pos > 0)
-    {
-        size_fp = pos * dest->_element_size;
-        first_partition = dest->_data;
-        current_bpos += size_fp;
-        memcpy(combined_array, first_partition, size_fp);
-    }
-
-    // Copy insert array into combined array
-    size_ip = insrt->_count * dest->_element_size;
-    memcpy((char*)combined_array + current_bpos, insrt->_data, size_ip);
-
-    // Copy second partition into combined array
-    // if requested position allows for it
-    if (pos < dest->_count)
-    {
-        size_sp = (dest->_count - pos) * dest->_element_size;
-        second_partition = (char*)dest->_data + size_fp;
-        current_bpos += size_ip;
-        memcpy((char*)combined_array + current_bpos, second_partition, size_sp);
-    }
-
-    free(dest->_data);
-    dest->_data = combined_array;
-    dest->_count = new_count;
-    dest->_capacity = new_count;
-
-    return 0;
+	dest->_count += src->_count;
+	return 0;
 }
 
-int da_shuffle(struct dynamic_array* da)
+int da_sort(DynamicArray* da, size_t count, size_t offset,
+            int (*compar)(const void*, const void*))
 {
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
+	if (da->_data == NULL)
+		return 1;
 
-    void* temp_copy;
-    void* temp_ptr1;
-    void* temp_ptr2;
-    temp_copy = malloc(da->_element_size);
-    size_t i;
-    for (i = 0; i < da->_count; ++i)
-    {
-        temp_ptr1 = da_at(da, i);
-        memcpy(temp_copy, temp_ptr1, da->_element_size);
-        temp_ptr2 = da_at(da, da_rand64() % da->_count);
-        memcpy(temp_ptr1, temp_ptr2, da->_element_size);
-        memcpy(temp_ptr2, temp_copy, da->_element_size);
-    }
+	qsort((char*)da->_data + (offset * da->_element_size), count, da->_element_size, compar);
 
-    free(temp_copy);
-
-    return 0;
-}
-
-int da_sort(struct dynamic_array* da, size_t count, size_t offset, int (*compar)(const void*, const void*))
-{
-    if (da->_data == NULL)
-    {
-        return 1;
-    }
-
-    qsort((char*)da->_data + (offset * da->_element_size), count, da->_element_size, compar);
-
-    return 0;
+	return 0;
 }
--- da.h
+++ da.h
@@ -1,28 +1,28 @@
-#ifndef __ArHPpZcTmq
-#define __ArHPpZcTmq
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
 
 #include <stddef.h>
 
-struct dynamic_array
+typedef struct DynamicArray
 {
-    size_t _count;
-    size_t _capacity;
-    size_t _element_size;
+	size_t _count;
+	size_t _capacity;
+	size_t _element_size;
 
-    size_t default_expand_amount;
+	size_t default_expand_amount;
 
-    void* _data;
-};
+	void* _data;
+} DynamicArray;
 
-int da_initialize(struct dynamic_array* da, size_t initial_capacity, size_t element_size);
-int da_free(struct dynamic_array* da);
-void* da_at(struct dynamic_array* da, size_t index);
-int da_expand(struct dynamic_array* da, size_t expand_amount);
-int da_shrink(struct dynamic_array* da);
-int da_push(struct dynamic_array* da, void* element);
-int da_pop(struct dynamic_array* da);
-int da_insert(struct dynamic_array* dest, struct dynamic_array* src, size_t pos);
-int da_shuffle(struct dynamic_array* da);
-int da_sort(struct dynamic_array* da, size_t count, size_t offset, int (*compar)(const void*, const void*));
+int da_initialize(DynamicArray* da, size_t initial_capacity, size_t element_size);
+int da_free(DynamicArray* da);
+void* da_at(DynamicArray* da, size_t index);
+int da_expand(DynamicArray* da, size_t expand_amount);
+int da_shrink(DynamicArray* da);
+int da_push(DynamicArray* da, void* element);
+int da_pop(DynamicArray* da);
+int da_insert(DynamicArray* dest, DynamicArray* src, size_t pos);
+int da_sort(DynamicArray* da, size_t count, size_t offset,
+            int (*compar)(const void*, const void*));
 
 #endif
